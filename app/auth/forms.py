5db6cd11a2626ce4a81ccd6874429c1b57866a0f
Machoka Daisy <machokadaisy25@gmail.com>
2020-09-27 01:11:18 +0300

    add custom validators to registration form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -9,3 +9,11 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
     password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('Oh no! This username has already been taken')
+
+    def validate_email(self,data_field):
+            if User.query.filter_by(email =data_field.data).first():
+                raise ValidationError('Oh no! This email account already exists')
