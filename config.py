ea34cfec97cd096d8df2512bc9f4f89eba3b2267
Machoka Daisy <machokadaisy25@gmail.com>
2020-09-26 21:09:04 +0300

    create database pitch and connect it to app

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
     contains general configuration code
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://daisy:4H@ppyfeet@localhost/pitch'
 
 class ProdConfig(Config):
     '''
