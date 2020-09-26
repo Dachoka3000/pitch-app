84cec7a1e458626bf01305ace46bd687ebd80faf
Machoka Daisy <machokadaisy25@gmail.com>
2020-09-27 00:09:49 +0300

    add password hashing

--- app/models.py
+++ app/models.py
@@ -9,7 +9,18 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(200), unique=True,index = True, nullable=False)
     email = db.Column(db.String(300),unique = True, index = True, nullable = False)
-    password = db.Column(db.String(50), nullable=False)
+    password_hash = db.Column(db.String(50), nullable=False)
+
+    @property
+    def password(self):
+        raise AttributeError('You are not permitted to view this password')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash,password)
 
     def __repr__(self):
         return f'User {self.username}'
