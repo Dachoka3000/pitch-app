2ae36833089f9a273172841d38f3d9993f236cf4
Machoka Daisy <machokadaisy25@gmail.com>
2020-09-27 03:07:56 +0300

    add routing for index page and profile page

---
+++ app/main/views.py
@@ -0,0 +1,18 @@
+from flask_login import login_required
+from flask import render_template, request, redirect, url_for,abort
+from . import main
+from ..models import User
+
+@main.route('/')
+def index():
+    
+    title = "Pitch Up"
+    return render_template('index.html', title = title)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    return render_template('profile.html', user = user)
